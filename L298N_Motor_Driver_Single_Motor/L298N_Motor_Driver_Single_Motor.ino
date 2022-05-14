70620b0dbb8ce88e8171042fa183e240d1469859
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-05-15 01:40:39 +0530

    single motor control

---
+++ L298N_Motor_Driver_Single_Motor/L298N_Motor_Driver_Single_Motor.ino
@@ -0,0 +1,24 @@
+#define s1  3
+#define s2  4
+#define switch_1 7
+
+
+void setup() 
+{
+  pinMode(s1,OUTPUT);
+  pinMode(s2,OUTPUT);
+}
+
+void loop() 
+{ 
+  if(digitalRead(switch_1) == HIGH)
+  { 
+    digitalWrite(s1,HIGH);
+    digitalWrite(s2,LOW);
+  }
+  else
+  { 
+    digitalWrite(s2,LOW);
+    digitalWrite(s1,HIGH); 
+  }
+}

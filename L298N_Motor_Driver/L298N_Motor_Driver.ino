beb846bfe62c63297ada08e2ac5600d4ba4ad394
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-05-15 17:17:55 +0530

    bug fix
    after hardware testing 
    bugs have been fixed

--- L298N_Motor_Driver/L298N_Motor_Driver.ino
+++ L298N_Motor_Driver/L298N_Motor_Driver.ino
@@ -2,7 +2,7 @@
 #define s2  4
 #define s3  5
 #define s4  6
-#define switch_1 7
+#define switch_1 9
 #define switch_2 8
 
 void setup() 
@@ -17,31 +17,24 @@ void setup()
 void loop() 
 { 
   if(digitalRead(switch_1) == HIGH)
-  {
-    if(digitalRead(switch_2) == HIGH)
-    { 
+  { 
       digitalWrite(s1,HIGH);
       digitalWrite(s2,LOW);
-    }
-    else
-    { 
-      digitalWrite(s3,HIGH);
-      digitalWrite(s4,LOW); 
-    }
-    
   }
   else
-  {
-     if(digitalRead(switch_2) == HIGH )
-    {
-      digitalWrite(s1,LOW);
+  { 
       digitalWrite(s2,HIGH);
-    }
-    else
-    { 
-      digitalWrite(s3,LOW);
-      digitalWrite(s4,HIGH); 
-    }
-     
+      digitalWrite(s1,LOW); 
+  }
+
+  if(digitalRead(switch_2) == HIGH )
+  {
+      digitalWrite(s4,LOW);
+      digitalWrite(s3,HIGH);
+  }
+  else
+  { 
+    digitalWrite(s3,LOW);
+    digitalWrite(s4,HIGH); 
   }
 }
